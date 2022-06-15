94e659253613b8156486945832d8bb56e3433efc
byteful <info@byteful.me>
2022-06-14 21:54:15 -0500

    Update 1.18 NMS impl to use better methods.

--- nms/v1_18_R2/src/main/java/me/byteful/lib/ocelot/impl/NMS_v1_18_R2.java
+++ nms/v1_18_R2/src/main/java/me/byteful/lib/ocelot/impl/NMS_v1_18_R2.java
@@ -3,19 +3,14 @@ package me.byteful.lib.ocelot.impl;
 import me.byteful.lib.ocelot.BlockPosition;
 import me.byteful.lib.ocelot.ChunkPosition;
 import me.byteful.lib.ocelot.OcelotHandler;
-import net.minecraft.network.protocol.game.ClientboundLevelChunkWithLightPacket;
-import net.minecraft.network.protocol.game.PacketPlayOutUnloadChunk;
-import net.minecraft.server.level.EntityPlayer;
 import net.minecraft.world.level.block.state.IBlockData;
 import net.minecraft.world.level.chunk.Chunk;
+import net.minecraft.world.level.chunk.ChunkSection;
 import net.minecraft.world.level.lighting.LightEngine;
-import org.bukkit.Bukkit;
 import org.bukkit.block.BlockState;
 import org.bukkit.craftbukkit.v1_18_R2.CraftChunk;
 import org.bukkit.craftbukkit.v1_18_R2.CraftWorld;
 import org.bukkit.craftbukkit.v1_18_R2.block.data.CraftBlockData;
-import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer;
-import org.bukkit.entity.Player;
 
 import java.util.Set;
 
@@ -27,8 +22,9 @@ public class NMS_v1_18_R2 implements OcelotHandler {
     final net.minecraft.core.BlockPosition bp = new net.minecraft.core.BlockPosition(state.getX(), state.getY(), state.getZ());
     final IBlockData ibd = ((CraftBlockData) state.getBlockData()).getState();
     chunk.i.remove(bp);
-    chunk.a(bp, ibd, false);
-    chunk.q.k().a(bp);
+    final ChunkSection cs = chunk.b(chunk.e(state.getY()));
+    cs.a(state.getX() & 15, state.getY() & 15, state.getZ() & 15, ibd);
+    //chunk.q.k().a(bp);
   }
 
   @Override
@@ -38,21 +34,22 @@ public class NMS_v1_18_R2 implements OcelotHandler {
     final CraftChunk chunkAt = (CraftChunk) world.getChunkAt(chunk.x, chunk.z);
 
     for (BlockPosition pos : blocks) {
-      engine.a(new net.minecraft.core.BlockPosition(pos.getX(), pos.getY(), pos.getZ()));
+      final net.minecraft.core.BlockPosition bp = new net.minecraft.core.BlockPosition(pos.getX(), pos.getY(), pos.getZ());
+      chunkAt.getHandle().q.k().a(bp);
+      engine.a(bp);
     }
 
-    final PacketPlayOutUnloadChunk unload = new PacketPlayOutUnloadChunk(chunk.x, chunk.z);
-    final ClientboundLevelChunkWithLightPacket load = new ClientboundLevelChunkWithLightPacket(chunkAt.getHandle(), engine, null, null, true);
-
-    for (Player p : Bukkit.getOnlinePlayers()) {
-      final EntityPlayer ep = ((CraftPlayer) p).getHandle();
-      final int dist = Bukkit.getViewDistance() + 1;
-      final ChunkPosition playerPos = new ChunkPosition(p.getLocation().getChunk());
-      if (chunk.distance(playerPos) > dist) {
-        continue;
-      }
-      ep.b.a(unload);
-      ep.b.a(load);
-    }
+//    final PacketPlayOutUnloadChunk unload = new PacketPlayOutUnloadChunk(chunk.x, chunk.z);
+//
+//    for (Player p : Bukkit.getOnlinePlayers()) {
+//      final EntityPlayer ep = ((CraftPlayer) p).getHandle();
+//      final int dist = Bukkit.getViewDistance() + 1;
+//      final ChunkPosition playerPos = new ChunkPosition(p.getLocation().getChunk());
+//      if (chunk.distance(playerPos) > dist) {
+//        continue;
+//      }
+//      ep.b.a(unload);
+//    }
+    world.refreshChunk(chunk.x, chunk.z);
   }
 }
